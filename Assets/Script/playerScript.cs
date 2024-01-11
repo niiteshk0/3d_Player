1a201b70d3f83e9d7eca933a51ed0e20c713d24b
Nitesh Kumar <nitesh.kgupta2002@gmail.com>
2024-01-11 18:35:11 +0530

    add player movement

--- Assets/Script/playerScript.cs
+++ Assets/Script/playerScript.cs
@@ -8,9 +8,34 @@ public class playerScipt : MonoBehaviour
     public float playerSpeed = 2f;
     public CharacterController characterController;
 
+    [Header("Ground Check")]
+    public float gravity = -9.81f;
+    public float jumpForce = 1f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    bool isGround;
+    public float groundDistance = 0.4f;
+    public LayerMask groundLayerMask;
+
+    [Header("For cinemachine Variables")]
+    public Transform playerCamera;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelcity;
+
     private void Update()
     {
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);      // ground Check code 
+
+        if(isGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);              // till here code of Ground Check
+
+
         playerMovement();
+        Jump();
     }
 
     void playerMovement()
@@ -22,7 +47,20 @@ public class playerScipt : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            characterController.Move(direction.normalized * playerSpeed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelcity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
         }
     }
 }
